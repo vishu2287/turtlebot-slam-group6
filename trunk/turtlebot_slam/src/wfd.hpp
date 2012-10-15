b0d77ef56173c4a7a898c4e2a5a77e0c8ab99468
lukaskang@gmail.com <lukaskang@gmail.com@4c2d594f-b965-8fc3-7555-3e2bcac766cd>
2012-10-15 22:39:09 +0000

    Everything cleaner now. wfd works.

---
+++ trunk/turtlebot_slam/src/wfd.hpp
@@ -0,0 +1,12 @@
+#include "ros/ros.h"
+#include <cstdlib> // Needed for rand()
+#include <ctime>
+#include <queue>
+#include "nav_msgs/OccupancyGrid.h"
+
+
+
+std::vector<std::vector<int> > frontierDetection(const nav_msgs::OccupancyGrid& grid, int x, int y);
+bool isFrontier(int x, int y);
+std::vector<int> getNeighbors(const nav_msgs::OccupancyGrid& grid, int x, int y);
+int getMapValue(const nav_msgs::OccupancyGrid& grid, int x, int y);
