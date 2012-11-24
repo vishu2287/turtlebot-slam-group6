019e4a0066075dd12c9721f1ec22c461b35cae1b
trinno89@gmail.com <trinno89@gmail.com@4c2d594f-b965-8fc3-7555-3e2bcac766cd>
2012-11-24 10:23:13 +0000

    First 2 lines of correspondence test finished.

--- turtlebot_graph_slam/src/correspondence_test.cpp
+++ turtlebot_graph_slam/src/correspondence_test.cpp
@@ -16,6 +16,10 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 	std::cout << "sigma = \n" << sigma << std::endl;
 
 	int t = 2; // time
+
+	/*
+	 * Create tau for both features j and k
+	 */
 	std::vector<int> tauJK;
 	int row = (t + 1 + j) * 3;
 	for (int feature = 0; feature < 2; feature++) {
@@ -35,7 +39,10 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 				}
 			}
 			if (!allZeros) {
-				tauJK.push_back(i);
+				// Add only if tauJ doesnt already contain the element
+				if (std::find(tauJK.begin(), tauJK.end(), i) == tauJK.end()) {
+					tauJK.push_back(i);
+				}
 			}
 			i++;
 		}
@@ -46,6 +53,12 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 //	for (int i = 0; i < tauJK.size(); i++) {
 //		std::cout << tauJK.at(i) << std::endl;
 //	}
+
+	/*
+	 * ----------------------------------------------------------
+	 * LINE 2 OF ALGORITHM PAGE 364
+	 * ----------------------------------------------------------
+	 */
 
 	//CONSTRUCT SIGMA Tau(j,k),Tau(j,k)
 	MatrixXd sigmapart = MatrixXd::Zero(tauJK.size() * 3, tauJK.size() * 3);
@@ -59,17 +72,21 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 
 	//CONSTRUCT Omegajk,Tau(j,k)
 	MatrixXd omega_jk_right = MatrixXd::Zero(6, tauJK.size() * 3);
-	for (int z = 0; z < tauJK.size(); z += 1) {
+	for (int z = 0; z < tauJK.size(); z++) {
 		//Assumption the jk in Omega jk,Tau(j,k) means we want position j and k resulting in a 6x(Tau(j,k)*3) long matrix
 		omega_jk_right.block(0, z * 3, 3, 3) += omega.block(j, tauJK[z], 3, 3);
 		omega_jk_right.block(3, z * 3, 3, 3) += omega.block(k, tauJK[z], 3, 3);
 	}
+//	std::cout << "omega_jk_right = \n" << omega_jk_right << std::endl;
+
 	//CONSTRUCT Tau(j,k),Omegajk
 	MatrixXd omega_jk_left = MatrixXd::Zero(tauJK.size() * 3, 6);
-	for (int z = 0; z < tauJK.size(); z += 1) {
+	for (int z = 0; z < tauJK.size(); z++) {
 		omega_jk_left.block(z * 3, 0, 3, 3) += omega.block(tauJK[z], j, 3, 3);
 		omega_jk_left.block(z * 3, 3, 3, 3) += omega.block(tauJK[z], k, 3, 3);
 	}
+//	std::cout << "omega_jk_left = \n" << omega_jk_left << std::endl;
+
 //	std::cout << "Omega times Sigma times Omega?" << std::endl
 //			<< omega_jk_right * sigmapart * omega_jk_left << std::endl; //ends in a 6x6 Matrix!
 
@@ -78,18 +95,50 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 	omega_jk_jk.block(0, 3, 3, 3) += omega.block(j, k, 3, 3);
 	omega_jk_jk.block(3, 0, 3, 3) += omega.block(k, j, 3, 3);
 	omega_jk_jk.block(3, 3, 3, 3) += omega.block(k, k, 3, 3);
+//	std::cout << "omega_jk_jk = \n" << omega_jk_jk << std::endl;
 
 	//Assumption : Omega[j,k] = 6x6 matrix ...
-	MatrixXd omega_j_k = MatrixXd::Zero(6, 6);
-	omega_j_k = omega_jk_jk - omega_jk_right * sigmapart * omega_jk_left;
-//	std::cout << "Omega j_k" << std::endl << omega_j_k << std::endl;
+	MatrixXd omega_j_k = omega_jk_jk
+			- omega_jk_right * sigmapart * omega_jk_left;
+//	std::cout << "omega_j_k = \n" << omega_j_k << std::endl;
+
+	/*
+	 * ----------------------------------------------------------
+	 * LINE 2 FINISHED
+	 * ----------------------------------------------------------
+	 */
+
+	/*
+	 * ----------------------------------------------------------
+	 * LINE 3 OF ALGORITHM PAGE 364
+	 * ----------------------------------------------------------
+	 */
+
 	//Construct mu j,k as seen on page 367
-	MatrixXd omega_jk_jk_inverse = MatrixXd::Zero(6, 6);
-	omega_jk_jk_inverse = omega_jk_jk.inverse();
+	MatrixXd omega_jk_jk_inverse = omega_jk_jk.inverse();
+	std::cout << "omega_jk_jk_inverse = \n" << omega_jk_jk_inverse << std::endl;
 	VectorXd mu_taujk = VectorXd::Zero(tauJK.size() * 3);
 	for (int z = 0; z < tauJK.size(); z += 1) {
-		//mu_taujk.block(z*3,1,3,1) += mu.block(tau[z],1,3,1);		// TODO mu_taujk sinnvoll füllen
+		mu_taujk.block(z * 3, 0, 3, 1) += mu.block(tauJK[z] * 3, 0, 3, 1);
 	}
+//	std::cout << "mu_taujk = \n" << mu_taujk << std::endl;
+	VectorXd xi_j_k = VectorXd::Zero(6);
+	row = (t + 1 + j) * 3;
+	for (int z = 0; z < 2; z++) {
+		xi_j_k.block(z * 3, 0, 3, 1) += xi.block(row, 0, 3, 1);
+		row = (t + 1 + k) * 3;
+	}
+//	std::cout << "xi_j_k = \n" << xi_j_k << std::endl;
+	MatrixXd mu_j_k = omega_jk_jk_inverse*(xi_j_k+omega_jk_right*mu_taujk);
+	std::cout << "mu_j_k = \n" << mu_j_k << std::endl;
+
+	VectorXd xi_j_k = omega_j_k*mu_j_k;
+	/*
+	 * ----------------------------------------------------------
+	 * LINE 3 FINISHED
+	 * ----------------------------------------------------------
+	 */
+
 //	std::cout << "Test" << std::endl << mu_taujk << std::endl;
 	return omega;
 
