2265901a5d931786fd98b4e5edd54e44d0304cb8
trinno89@gmail.com <trinno89@gmail.com@4c2d594f-b965-8fc3-7555-3e2bcac766cd>
2012-11-25 12:55:54 +0000

    Correspondence test finished. Needs to be tested with actual matrices. So far its working fine.

--- trunk/turtlebot_graph_slam/src/correspondence_test.cpp
+++ trunk/turtlebot_graph_slam/src/correspondence_test.cpp
@@ -5,11 +5,19 @@ using namespace Eigen;
 //Table 11.4 Page 349
 //@TODO: Implement
 //TODO Make COV_SIGMA to a list of cov sigma according to time
-MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
+double correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 		MatrixXd sigma, int j, int k) {
 
 	ROS_INFO("CORRESPONDENCE TEST CALLED");
+//	for(int i = 0; i<omega.rows(); i++)
+//	{
+//		for(int j = 0; j<omega.cols(); j++)
+//		{
+//			omega(i, j) = i*1000+j;
+//		}
+//	}
 	omega.block(3 * 3, 2 * 3, 3, 3) = MatrixXd::Zero(3, 3); // for testing purposes
+	omega.block(2 * 3, 3 * 3, 3, 3) = MatrixXd::Zero(3, 3); // for testing purposes
 	std::cout << "omega = \n" << omega << std::endl;
 	std::cout << "xi = \n" << xi << std::endl;
 	std::cout << "mu = \n" << mu << std::endl;
@@ -64,39 +72,37 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 	MatrixXd sigmapart = MatrixXd::Zero(tauJK.size() * 3, tauJK.size() * 3);
 	for (int z = 0; z < tauJK.size(); z++) {
 		for (int x = 0; x < tauJK.size(); x++) {
-			sigmapart.block(z * 3, x * 3, 3, 3) += sigma.block(tauJK[z],
-					tauJK[z], 3, 3);
+			sigmapart.block(z * 3, x * 3, 3, 3) += sigma.block(tauJK[z]*3,
+					tauJK[x]*3, 3, 3);
 		}
 	}
 //	std::cout << "sigmaPart = \n" << sigmapart << std::endl;
 
 	//CONSTRUCT Omegajk,Tau(j,k)
-	MatrixXd omega_jk_right = MatrixXd::Zero(6, tauJK.size() * 3);
+	MatrixXd omega_jk_tauJK = MatrixXd::Zero(6, tauJK.size() * 3);
 	for (int z = 0; z < tauJK.size(); z++) {
 		//Assumption the jk in Omega jk,Tau(j,k) means we want position j and k resulting in a 6x(Tau(j,k)*3) long matrix
-		omega_jk_right.block(0, z * 3, 3, 3) += omega.block(j, tauJK[z], 3, 3);
-		omega_jk_right.block(3, z * 3, 3, 3) += omega.block(k, tauJK[z], 3, 3);
+		omega_jk_tauJK.block(0, z * 3, 3, 3) = omega.block(3*(t+1+j), tauJK[z]*3, 3, 3);
+		omega_jk_tauJK.block(3, z * 3, 3, 3) = omega.block(3*(t+1+k), tauJK[z]*3, 3, 3);
 	}
-//	std::cout << "omega_jk_right = \n" << omega_jk_right << std::endl;
+//	std::cout << "omega_jk_tauJK = \n" << omega_jk_tauJK << std::endl;
 
 	//CONSTRUCT Tau(j,k),Omegajk
-	MatrixXd omega_jk_left = MatrixXd::Zero(tauJK.size() * 3, 6);
+	MatrixXd omega_tauJK_jk = MatrixXd::Zero(tauJK.size() * 3, 6);
 	for (int z = 0; z < tauJK.size(); z++) {
-		omega_jk_left.block(z * 3, 0, 3, 3) += omega.block(tauJK[z], j, 3, 3);
-		omega_jk_left.block(z * 3, 3, 3, 3) += omega.block(tauJK[z], k, 3, 3);
+		omega_tauJK_jk.block(z * 3, 0, 3, 3) = omega.block(tauJK[z]*3, 3*(t+1+j), 3, 3);
+		omega_tauJK_jk.block(z * 3, 3, 3, 3) = omega.block(tauJK[z]*3, 3*(t+1+k), 3, 3);
 	}
-//	std::cout << "omega_jk_left = \n" << omega_jk_left << std::endl;
+//	std::cout << "omega_tauJK_jk = \n" << omega_tauJK_jk << std::endl;
 
 	MatrixXd omega_jk_jk = MatrixXd::Zero(6, 6);
-	omega_jk_jk.block(0, 0, 3, 3) += omega.block(j, j, 3, 3); //Omega jk_jk = Omegaj,j ; Omega j,k ; Omega k,j; Omega k,k ??
-	omega_jk_jk.block(0, 3, 3, 3) += omega.block(j, k, 3, 3);
-	omega_jk_jk.block(3, 0, 3, 3) += omega.block(k, j, 3, 3);
-	omega_jk_jk.block(3, 3, 3, 3) += omega.block(k, k, 3, 3);
+	omega_jk_jk.block(0, 0, 3, 3) += omega.block(3*(t+1+j), 3*(t+1+j), 3, 3); //Omega jk_jk = Omegaj,j ; Omega j,k ; Omega k,j; Omega k,k ??
+	omega_jk_jk.block(0, 3, 3, 3) += omega.block(3*(t+1+j), 3*(t+1+k), 3, 3);
+	omega_jk_jk.block(3, 0, 3, 3) += omega.block(3*(t+1+k), 3*(t+1+j), 3, 3);
+	omega_jk_jk.block(3, 3, 3, 3) += omega.block(3*(t+1+k), 3*(t+1+k), 3, 3);
 //	std::cout << "omega_jk_jk = \n" << omega_jk_jk << std::endl;
 
-	//Assumption : Omega[j,k] = 6x6 matrix ...
-	MatrixXd omega_j_k = omega_jk_jk
-			- omega_jk_right * sigmapart * omega_jk_left;
+	MatrixXd omega_j_k = omega_jk_jk - omega_jk_tauJK * sigmapart * omega_tauJK_jk;
 //	std::cout << "omega_j_k = \n" << omega_j_k << std::endl;
 
 	/*
@@ -113,12 +119,14 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 
 	//Construct mu j,k as seen on page 367
 	MatrixXd omega_jk_jk_inverse = omega_jk_jk.inverse();
-	std::cout << "omega_jk_jk_inverse = \n" << omega_jk_jk_inverse << std::endl;
+//	std::cout << "omega_jk_jk_inverse = \n" << omega_jk_jk_inverse << std::endl;
+
 	VectorXd mu_taujk = VectorXd::Zero(tauJK.size() * 3);
-	for (int z = 0; z < tauJK.size(); z += 1) {
+	for (int z = 0; z < tauJK.size(); z ++) {
 		mu_taujk.block(z * 3, 0, 3, 1) += mu.block(tauJK[z] * 3, 0, 3, 1);
 	}
 //	std::cout << "mu_taujk = \n" << mu_taujk << std::endl;
+
 	VectorXd xi_j_k = VectorXd::Zero(6);
 	row = (t + 1 + j) * 3;
 	for (int z = 0; z < 2; z++) {
@@ -126,10 +134,12 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 		row = (t + 1 + k) * 3;
 	}
 //	std::cout << "xi_j_k = \n" << xi_j_k << std::endl;
-	MatrixXd mu_j_k = omega_jk_jk_inverse*(xi_j_k+omega_jk_right*mu_taujk);
-	std::cout << "mu_j_k = \n" << mu_j_k << std::endl;
+
+	MatrixXd mu_j_k = omega_jk_jk_inverse*(xi_j_k+omega_jk_tauJK*mu_taujk);
+//	std::cout << "mu_j_k = \n" << mu_j_k << std::endl;
 
 	xi_j_k = omega_j_k*mu_j_k;
+//	std::cout << "xi_j_k = \n" << xi_j_k << std::endl;
 
 	/*
 	 * ----------------------------------------------------------
@@ -142,17 +152,17 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 	 * LINE 4 OF ALGORITHM PAGE 364
 	 * ----------------------------------------------------------
 	 */
-	MatrixXd unity_one_mone = MatrixXd::Zero(6, 3);
-	 unity_one_mone << 1, 1, 1, 
-			   1, 1, 1,
-			   1, 1, 1,
-			   -1,-1,-1,
-			   -1,-1,-1,
-			   -1,-1,-1;
+	MatrixXd unity_one = MatrixXd::Zero(6, 3); // Why three columns?
+	 unity_one <<
+			 1, 0, 0,
+			 0, 1, 0,
+			 0, 0, 1,
+			-1, 0, 0,
+			 0,-1, 0,
+			 0, 0,-1;
 
-	
-	std::cout << "Test" << std::endl << unity_one_mone.transpose() * omega_j_k * unity_one_mone << std::endl;
-	//Where to save that ?  
+	MatrixXd omega_deltaJ_k = unity_one.transpose() * omega_j_k * unity_one;
+//	std::cout << "omega_deltaJ_k = \n" << omega_deltaJ_k << std::endl;
 	/*
 	 * ----------------------------------------------------------
 	 * LINE 4 FINISHED
@@ -163,7 +173,8 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 	 * LINE 5 OF ALGORITHM PAGE 364
 	 * ----------------------------------------------------------
 	 */
-	std::cout << "Test" << std::endl << unity_one_mone.transpose()* xi_j_k << std::endl;
+	VectorXd xi_deltaJ_k = unity_one.transpose()* xi_j_k;
+//	std::cout << "xi_deltaJ_k = \n" << xi_deltaJ_k << std::endl;
 	/*
 	 * ----------------------------------------------------------
 	 * LINE 5 FINISHED
@@ -174,6 +185,8 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 	 * LINE 6 OF ALGORITHM PAGE 364
 	 * ----------------------------------------------------------
 	 */
+	VectorXd mu_deltaJ_k = omega_deltaJ_k.inverse() * xi_deltaJ_k;
+//	std::cout << "mu_deltaJ_k = \n" << mu_deltaJ_k << std::endl;
 	/*
 	 * ----------------------------------------------------------
 	 * LINE 6 FINISHED
@@ -184,14 +197,24 @@ MatrixXd correspondence_test(MatrixXd omega, VectorXd xi, VectorXd mu,
 	 * LINE 7 OF ALGORITHM PAGE 364
 	 * ----------------------------------------------------------
 	 */
-	const double PI = 3.141592;
-	// double tmp = pow(5,2); potenz
-	// exp(1) = E hoch 1
-	std::cout << "Test" << std::endl << exp(1) << std::endl;
-	//((abs(2*PI*Inverse))^-0.5)*exp(-0.5*mujk*omegajk*mujk)
-	// @TODO
+	MatrixXd m = 2*M_PI*omega_deltaJ_k.inverse();
+	std::cout << "m = \n" << m << std::endl;
 
-//	std::cout << "Test" << std::endl << mu_taujk << std::endl;
-	return omega;
+	double determinant =
+			m(0,0)*m(1,1)*m(2,2)+
+			m(0,1)*m(1,2)*m(2,0)+
+			m(0,2)*m(1,0)*m(2,1)-
+			m(0,2)*m(1,1)*m(2,2)-
+			m(0,1)*m(1,0)*m(2,2)-
+			m(0,0)*m(1,2)*m(2,1);
+	determinant = abs(determinant); // @TODO: Check if this is correct or neccessary
+
+	double normalizer = pow(determinant, -0.5);
+
+	double exponent = -0.5*mu_deltaJ_k.transpose()*omega_deltaJ_k.inverse()*mu_deltaJ_k;
+
+	double e = exp(exponent);
+
+	return normalizer*e;
 
 }
