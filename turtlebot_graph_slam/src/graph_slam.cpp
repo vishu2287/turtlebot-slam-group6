dd18804aead92b31de2318fad23bb3488704d40c
lukaskang@gmail.com <lukaskang@gmail.com@4c2d594f-b965-8fc3-7555-3e2bcac766cd>
2012-12-07 15:44:17 +0000

    screenshot optimization

--- turtlebot_graph_slam/src/graph_slam.cpp
+++ turtlebot_graph_slam/src/graph_slam.cpp
@@ -37,11 +37,72 @@ MatrixXd graph_slam (MatrixXd u, std::vector<MatrixXd> z, int deltaT) {
 	// Call initialize method
 	MatrixXd muPath = graph_init(u, deltaT);
 	// std::cout << "muPath = \n" << muPath << std::endl;
+	
+/*
+*
+*------------------------------------------------------------------this is a part of linearize for testing
+* the mu vector is filled with the laser poses and transformed into the mu output format
+*
+*/
+for(int t = 0; t<u.cols(); t++)
+    {
+	for (int i = 0; i < z[t].cols(); i++)
+       {
+           // line 13
+           // each c[t] as one row, like a transposed vector
+           // mu has u.cols() + 1 columns of robot poses.
+           int j = c[t](0, i);
 
+           //Calculating x and y coordinate of the features and adding them to mu
+           if(muPath.cols() < u.cols()+ 1 + j+1) {
+          // std::cout << "Adding cols for z["<< t <<"]"<< std::endl;
+              MatrixXd newMu(3, muPath.cols() + z[t].cols());
+              newMu.block(0, 0, 3, muPath.cols()) = muPath;
+              int index = muPath.cols();
+              for (int k = 0; k < z[t].cols(); k++) {
+
+                Vector3d feature = z[t].col(k);
+                // x     =    r       * cos(phi        - theta      ) + x of pos
+                double x = feature(0) * cos(feature(1) - muPath(2, t)) + muPath(0, t);
+                // y     =    r       * sin(phi        - theta      ) + y of pos
+                double y = feature(0) * sin(feature(1) - muPath(2, t)) + muPath(1, t);
+//                std::cout << "X = " << x << "\n"<<std::endl;
+//                std::cout << "Y = " << y << "\n"<<std::endl;
+                newMu(0, index) = x;
+                newMu(1, index) = y;
+                newMu(2, index) = 1;
+                //Only the first and the second element of the feature entries in mu are filled. The orientation is always 0.
+                index++;
+             }
+             muPath = newMu;
+             // finalMu = newMu;
+           }
+       }
+   }
+    int numberLandmarks = 0;
+   for (int i = 0; i < z.size(); i++) {
+	   numberLandmarks += z[i].cols();
+   }
+      VectorXd outMu = VectorXd::Zero(3*(u.cols() + 1 + numberLandmarks), 1);
+   for(int i = 0; i < outMu.rows(); i++) {
+        int index = (int)(i/3);
+        outMu(i) = muPath(0,index);
+        outMu(i+1) = muPath(1, index);
+        outMu(i+2) = muPath(2, index);
+        i+=2;
+   }
+       // std::cout << "out mu = \n" << outMu << std::endl;
+
+
+   /*
+   *
+   *------------------------------------------------------------------end of linearize code
+  */
     // Call linearize method
-    MatrixXd omegaAndXi = linearize(u, z, c, muPath, deltaT);
-    MatrixXd omega = omegaAndXi.topLeftCorner(omegaAndXi.rows(), omegaAndXi.rows());
-    MatrixXd xi = omegaAndXi.topRightCorner(omegaAndXi.rows(),1);
+    // MatrixXd omegaAndXi = linearize(u, z, c, muPath, deltaT);
+    // MatrixXd omega = omegaAndXi.topLeftCorner(omegaAndXi.rows(), omegaAndXi.rows());
+    // MatrixXd xi = omegaAndXi.topRightCorner(omegaAndXi.rows(),1);
+
     // std::cout << "omega =  \n" << omega << std::endl;
     // std::cout << "xi =  \n" << xi << std::endl;
 
@@ -73,6 +134,6 @@ MatrixXd graph_slam (MatrixXd u, std::vector<MatrixXd> z, int deltaT) {
 //	std::cout << "correspondenceProbability = \n" << correspondenceProbability
 //			<< std::endl;
 
-	return xi; // should return full mu
+	return outMu; // should return full mu
 }
 
