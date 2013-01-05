0713515d0c592d882d06348f84e53dda5fbb98dd
trinno89@gmail.com <trinno89@gmail.com@4c2d594f-b965-8fc3-7555-3e2bcac766cd>
2013-01-05 11:28:29 +0000

    Finished implementing significant parts of the new algorithm.

--- turtlebot_graph_slam/include/GraphBasedAlgo1.hpp
+++ turtlebot_graph_slam/include/GraphBasedAlgo1.hpp
@@ -2,4 +2,4 @@
 #include <Eigen/Dense>
 using namespace Eigen;
 
-MatrixXd algorithm1 (VectorXd x, std::vector<MatrixXd> e_i_j, std::vector<MatrixXd> omega_i_j) ;
+MatrixXd algorithm1 (VectorXd x, std::vector<Vector3d> z, std::vector<Matrix3d> omega, std::vector<int> is, std::vector<int> js) ;
