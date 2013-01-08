131b53840091734aa756b853cf73bd3efbde7ae4
trinno89@gmail.com <trinno89@gmail.com@4c2d594f-b965-8fc3-7555-3e2bcac766cd>
2013-01-08 23:01:49 +0000

    Fixed a major bug in the occupancy grid and added rotation!

--- turtlebot_graph_slam/src/OccupancyGrid.cpp
+++ turtlebot_graph_slam/src/OccupancyGrid.cpp
@@ -23,6 +23,7 @@ nav_msgs::OccupancyGrid initializeOccupancyGrid(int length, double resolution) {
     og.header.stamp = ros::Time::now();
     og.info.width = SIZE;
     og.info.height = SIZE;
+    ROS_INFO_STREAM("MAP HEIGHT : " << og.info.height);
     og.data.resize(SIZE * SIZE);
 
     return og;
@@ -38,35 +39,44 @@ nav_msgs::OccupancyGrid updateOccupancyGrid(nav_msgs::OccupancyGrid og, std::vec
         for(int i = 0; i < og.data.size();i++){
 		og.data[i] = -1;
 	}
-	double xtranspose = 0;
-	double ytranspose = 0;
 	for(int i = 0 ; i < laserscansaver.size() ; i++){
 		sensor_msgs::PointCloud temp = lasertrans(laserscansaver[i]);
 		if(i>= poses.size())
 			break;
-	xtranspose+=poses[i](0,0);
-	ytranspose+=poses[i](1,0);
-	//ROS_INFO_STREAM("Occupancy Grid has height: "<<xtranspose<<" and width: "<<ytranspose);
+        double xPose = poses[i](0,0);
+        double yPose = poses[i](1,0);
+        double yaw = poses[i](2,0);
+
 		for(int z = 0; z < temp.points.size() ; z++){
-			        int grid_x = (unsigned int)(((temp.points[z].x+xtranspose)/RESOLUTION + SIZE/2));
-   			        int grid_y = (unsigned int)(((temp.points[z].y+ytranspose)/RESOLUTION + SIZE/2));
-       			        og.data[((grid_y*og.info.width)+grid_x)] = 100;
+
+            // Equation to solve: A*(x-p)+p
+            // In order to have the points of the measurement rotated the same way the robot did
+
+            // Initialised position of the points on the grid, but without rotation
+            int grid_x = (int)((temp.points[z].x+xPose)/RESOLUTION + SIZE/2);
+            int grid_y = (int)((temp.points[z].y+yPose)/RESOLUTION + SIZE/2);
+
+            // Now apply the equation mentioned above to rotate around the robot
+            double a11 =  cos(yaw);
+            double a12 = -sin(yaw);
+            double a21 =  sin(yaw);
+            double a22 =  cos(yaw);
+
+            double x1 = grid_x;
+            double x2 = grid_y;
+
+            double p1 = xPose/RESOLUTION + SIZE/2;
+            double p2 = yPose/RESOLUTION + SIZE/2;
+
+            double diffX = x1-p1;
+            double diffY = x2-p2;
+
+            grid_x = a11*diffX + a12*diffY + p1;
+            grid_y = a21*diffX + a22*diffY + p2;
+
+            og.data[((grid_y*og.info.width)+grid_x)] = 100;
 		}
+
 	}
-    /*for(int pose = (t+1)*3; pose < mu.size(); pose += 3) {
-        double x = mu(pose);
-        if(x < -10 || x > 10){ //There are sometimes values of 30000000 and - 30000000 ... for testing
-            x = 0;
-        }
-        double y = mu(pose+1);
-        if(y < -10 || y > 10){ //There are sometimes values of 30000000 and - 30000000 ... Otherwise the occupancy grid WONT work
-            y = 0;
-        }
-        double z = mu(pose+2);
-        int grid_x = (unsigned int)((x/RESOLUTION + SIZE/2));
-        int grid_y = (unsigned int)((y/RESOLUTION + SIZE/2));
-        //ROS_INFO_STREAM("Grid X = "<< grid_x << ", Grid Y =" << grid_y);
-        og.data[((grid_y*og.info.width)+grid_x)] = 100;
-    }*/
     return og;
 }
