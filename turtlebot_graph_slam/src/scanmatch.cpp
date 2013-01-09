aa97dea94d1815596e5fc02f24492bb3832c5307
lukaskang@gmail.com <lukaskang@gmail.com@4c2d594f-b965-8fc3-7555-3e2bcac766cd>
2013-01-09 14:38:11 +0000

    Scan_match cleaned and fixed. The output makes sense now most of the time. Paramters tuned according to few experiments. There might be a better configuration.

--- turtlebot_graph_slam/src/scanmatch.cpp
+++ turtlebot_graph_slam/src/scanmatch.cpp
@@ -15,196 +15,227 @@ using namespace Eigen;
 //TODO FIX LASERSCAN SIZE so both scans have the same number of scans
 
 double getDistance(geometry_msgs::Point32 one, geometry_msgs::Point32 two) {
-	return sqrt(pow((one.x - two.x), 2) + pow((one.y - two.y), 2));
+    return sqrt(pow((one.x - two.x), 2) + pow((one.y - two.y), 2));
 }
 
-MatrixXd scanmatch(sensor_msgs::PointCloud one ,  sensor_msgs::PointCloud two){
-MatrixXd robotpos = MatrixXd::Zero(3,1);
- ROS_INFO_STREAM("Scanmatcher is starting...");
-	//while ! converging  			//START of the icp iterations
-	int secondsize = two.points.size();
-	int saver[two.points.size()];
-	bool errors[two.points.size()];
-	double delta = 0.0005;
-	double distance = 1000;
-	double oldDistance = 10000;
-	double distances[two.points.size()];
-	double robotx = 0;
-	double roboty = 0;
-	double robotphi = 0;
-	double MAX_DISTANCE = 4;
-	//for(int runthroughs = 0; runthroughs < Threshold ; runthroughs++){
-		while(abs(oldDistance - distance) > delta) {
-			oldDistance = distance;
-			distance = 0;
-		int numberofpairs = 0;
-		 // SEARCH CLOSEST NEIGHBOR
-		 //-----------------------------------------------------------------------
-		 for(int i = 0; i < two.points.size();i++){
-			double prev = 1000000;
-			for(int z = 0; z < one.points.size();z++){
-				double length = getDistance(one.points[z], two.points[i]);// sqrt(pow(one.points[z].x-two.points[i].x,2)+pow(one.points[z].y-two.points[i].y,2));
-				if(length < prev){
-					if(length >= MAX_DISTANCE){
-						errors[i] = true;
-						
-					} else {
-					//Save position of closest neighbor here
-					saver[i] = z;
-					distances[i] = length;	
-					numberofpairs++;
-					}
-					prev = length;
-				}
-			}
+MatrixXd scanmatch(sensor_msgs::PointCloud model ,  sensor_msgs::PointCloud target){
+    MatrixXd robotpos = MatrixXd::Zero(3,1);
+    ROS_INFO_STREAM("Scanmatcher is starting...");
+    //while ! converging  			//START of the icp iterations
+    int modelSize = model.points.size();
+    int saver[modelSize];
+    int its = 0;
+    int maxIts = 500;
+    bool errors[modelSize];
+    double delta = 0.02;
+    double distance = 0;
+    double oldDistance = 1000;
+    double robotx = 0;
+    double roboty = 0;
+    double robotphi = 0;
+    double MAX_DISTANCE = 0.5;
+    double distanceDiff = 100;
+    double centroidTargetX = 0;
+    double centroidTargetY = 0;
+    double centroidModelX = 0;
+    double centroidModelY = 0;
+    double modelX = 0;
+    double modelY = 0;
+    double targetX = 0;
+    double targetY = 0;
+    double distances[modelSize];
+    MatrixXd Mtd;
+    MatrixXd Mmd;
+    Matrix3d Tmat;
+    MatrixXd H;
+    Matrix2d U;
+    Matrix2d V;
+    Matrix2d R;
+    Vector2d centerOfModel;
+    Vector2d centerOfTarget;
+    Vector2d t;
+    Vector3d temp_coord;
+    //for(int runthroughs = 0; runthroughs < Threshold ; runthroughs++){
+    while(distanceDiff > delta && its <= maxIts) {
+        its++;
+        oldDistance = distance;
+        distance = 0;
+        int numberofpairs = 0;
+        // SEARCH CLOSEST NEIGHBOR
+        //-----------------------------------------------------------------------
+        for(int i = 0; i < model.points.size();i++){
+            double prev = 1000000;
+            for(int z = 0; z < target.points.size();z++){
+                double length = getDistance(target.points[z], model.points[i]);
+                if(length < prev){
+                    if(length >= MAX_DISTANCE){
+                        errors[i] = true;
 
-			//std::cout << "The nearest neighbour of point " << i << " in the second scan is point " << saver[i] << " in the first scan"<< "\n";
-		 }
-	
-		 for(int i = 0; i < two.points.size(); i++) {
-			if(!errors[i])
-		 	distance += distances[i];
-		 }
-		  std::cout << "Total distance = " << distance << "\n";
+                    } else {
+                        //Save position of closest neighbor here
+                        saver[i] = z;
+                        distances[i] = length;
 
-		// Calculate center points of both scans
-		// ----------------------------------------------------
-		 double centroidxone = 0;
-		 double centroidyone = 0;
-		 double centroidxtwo = 0;
-		 double centroidytwo = 0;
-		 double x = 0;
-		 double y = 0;
-		 double x1 = 0;
-		 double y1 = 0;
-		 for(int i = 0; i < two.points.size();i++){
-			if(!errors[i]){
-			x+= two.points[i].x;
-			y+= two.points[i].y;
-			x1+= one.points[saver[i]].x;
-			y1+= one.points[saver[i]].y;
-			}
-		 }
-		 centroidxtwo = x/numberofpairs;			//Checken ob es double bleibt
-		 centroidytwo = y/numberofpairs;
-		 centroidxone = x1/numberofpairs;
-		 centroidyone = y1/numberofpairs;
-		 ROS_INFO_STREAM("Correspondence and centroids created !");
-		// Calculate Correlation matrix H
-		// ----------------------------------------------------
-		MatrixXd Mtd = MatrixXd::Zero(2,numberofpairs);
-		MatrixXd Mmd = MatrixXd::Zero(2,numberofpairs);
-		int counter = 0;
-		 for(int i = 0; i < two.points.size();i++){
-			if(!errors[i]){
-			Mtd(0,i-counter) = two.points[i].x-centroidxtwo;
-			Mtd(1,i-counter) = two.points[i].y-centroidytwo;
-			Mmd(0,i-counter) = one.points[saver[i]].x-centroidxone;
-			Mmd(1,i-counter) = one.points[saver[i]].y-centroidyone;
-			} else {
-			counter++;
-			}
-		 }
-		MatrixXd H = Mmd*Mtd.transpose() ;
-		ROS_INFO_STREAM("Matrizes created!");
-		// Apply SVD (Singular value decomposition) TO GET rOTATION mATRIX r
-		// ----------------------------------------------------
-		 JacobiSVD<MatrixXd> svd(H,Eigen::ComputeThinU | Eigen::ComputeThinV);//(H);//SVD<MatrixXd> svdOfA(H);
-		 MatrixXd U = svd.matrixU();
-		 MatrixXd V = svd.matrixV();
-		 MatrixXd R = V*U.transpose();
-			//std::cout << "R" << R;
-		// Estimate translation vector and build final transformation matrix
-		// ----------------------------------------------------
-		//std::cout << "R" << R;
-		VectorXd centertwo(2);
-		VectorXd centerone(2);
-		centertwo(0) = centroidxtwo;
-		centertwo(1) = centroidytwo;
-		centerone(0) = centroidxone;
-		centerone(1) = centroidyone;
-		VectorXd t = centertwo-R*centerone;
-		MatrixXd Tmat =MatrixXd::Zero(3,3);
-		Tmat << R(0,0),R(0,1),t(0),
-			R(1,0),R(1,1),t(1),
-			0,0,1;
-		//std::cout << "Translation Matrix" << R;
-		// Final calculation
-		// ----------------------------------------------------
-		 for(int i = 0; i < one.points.size();i++){
-			Vector3d M(3);
-			M(0) = one.points[i].x;
-			M(1) = one.points[i].y;
-			M(2) = 1;
-			VectorXd sol = Tmat*M;
-			one.points[i].x = sol(0);
-			one.points[i].y = sol(1);
-		 }
-		Vector3d temp_coord(3);
-		temp_coord(0) = robotx;
-		temp_coord(1) = roboty;
-		temp_coord(2) = 1;
-		VectorXd robotsol = Tmat*temp_coord;
-		robotx = robotsol(0);
-		roboty = robotsol(1);
-		robotphi += acos(R(0,0));
-	}
+                        errors[i] = false;
+                    }
+                    prev = length;
+                }
+            }
+        }
+        for(int i = 0; i < modelSize; i++) {
+            if(!errors[i]) {
+                numberofpairs++;
+                distance += distances[i];
+            }
+        }
+//        std::cout << "Total distance = " << distance << "\n";
+        // Calculate center points of both scans
+        // ----------------------------------------------------
+        centroidTargetX = 0;
+        centroidTargetY = 0;
+        centroidModelX = 0;
+        centroidModelY = 0;
+        modelX = 0;
+        modelY = 0;
+        targetX = 0;
+        targetY = 0;
+        for(int i = 0; i < model.points.size();i++){
+            if(!errors[i]){
+                modelX+= model.points[i].x;
+                modelY+= model.points[i].y;
+                targetX+= target.points[saver[i]].x;
+                targetY+= target.points[saver[i]].y;
+            }
+        }
+        centroidModelX = modelX/numberofpairs;			//Checken ob es double bleibt
+        centroidModelY = modelY/numberofpairs;
+        centroidTargetX = targetX/numberofpairs;
+        centroidTargetY = targetY/numberofpairs;
+//        ROS_INFO_STREAM("Correspondence and centroids created !");
+        // Calculate Correlation matrix H
+        // ----------------------------------------------------
+        Mtd = MatrixXd::Zero(2,numberofpairs);
+        Mmd = MatrixXd::Zero(2,numberofpairs);
+        int counter = 0;
+        for(int i = 0; i < model.points.size();i++){
+            if(!errors[i]){
+                Mmd(0,i-counter) = model.points[i].x-centroidModelX;
+                Mmd(1,i-counter) = model.points[i].y-centroidModelY;
+                Mtd(0,i-counter) = target.points[saver[i]].x-centroidTargetX;
+                Mtd(1,i-counter) = target.points[saver[i]].y-centroidTargetY;
+            } else {
+                counter++;
+            }
+        }
+        H = Mmd*Mtd.transpose() ;
+//        ROS_INFO_STREAM("Matrizes created!");
+        // Apply SVD (Singular value decomposition) TO GET rOTATION mATRIX r
+        // ----------------------------------------------------
+        JacobiSVD<MatrixXd> svd(H,Eigen::ComputeThinU | Eigen::ComputeThinV);//(H);//SVD<MatrixXd> svdOfA(H);
+        U = svd.matrixU();
+        V = svd.matrixV();
+        R = V*U.transpose();
+        //std::cout << "R" << R;
+        // Estimate translation vector and build final transformation matrix
+        // ----------------------------------------------------
+//        std::cout << "R" << R;
+//         ROS_INFO_STREAM("SVD complete");
+        centerOfModel(0) = centroidModelX;
+        centerOfModel(1) = centroidModelY;
+        centerOfTarget(0) = centroidTargetX;
+        centerOfTarget(1) = centroidTargetY;
+        t = centerOfTarget - R * centerOfModel;
+        Tmat << R(0,0),R(0,1),t(0),
+                R(1,0),R(1,1),t(1),
+                0,       0,     1;
+//        std::cout << "Translation Matrix" << R;
+//        ROS_INFO_STREAM("Tmat created!");
+        // Final calculation
+        // ----------------------------------------------------
+        for(int i = 0; i < model.points.size();i++){
+            Vector3d M;
+            M(0) = model.points[i].x;
+            M(1) = model.points[i].y;
+            M(2) = 1;
+            Vector3d sol = Tmat * M;
+            model.points[i].x = sol(0);
+            model.points[i].y = sol(1);
+        }
 
-robotpos(0,0) = robotx;
-robotpos(1,0) = roboty;
-robotpos(2,0) = robotphi;
-return robotpos;
+        temp_coord(0) = robotx;
+        temp_coord(1) = roboty;
+        temp_coord(2) = 1;
+
+        Vector3d robotsol = Tmat * temp_coord;
+
+        robotx = robotsol(0);
+        roboty = robotsol(1);
+        robotphi += acos(R(0,0));
+
+        distanceDiff = (oldDistance - distance);
+//        std::cout << "distanceDiff :" << distanceDiff << "\n";
+        if(distanceDiff < 0) {
+            distanceDiff *= -1;
+//            ROS_ERROR("DISTANCE INCREASED!");
+        }
+//        std::cout << "distanceDiff :" << distanceDiff << "\n";
+    }
+    std::cout << "\nScan matcher took " << its << " iterations.\n";
+    robotpos(0,0) = robotx;
+    robotpos(1,0) = roboty;
+    robotpos(2,0) = robotphi;
+    return robotpos;
 }
 /*bool contains(int test [], int i){
-	for(int i= 0; i < sizeof(test) ; i++){
-		if(test[i] == i)
-			return true;
-	}
-	return false;
+    for(int i= 0; i < sizeof(test) ; i++){
+        if(test[i] == i)
+            return true;
+    }
+    return false;
 }
 sensor_msgs::PointCloud scanmatch(sensor_msgs::PointCloud one ,  sensor_msgs::PointCloud two){
  ROS_INFO_STREAM("Scanmatcher is starting...");
-	//while ! converging  			//START of the icp iterations
-	int secondsize = two.points.size();
-	int saver[two.points.size()];
-	double lengthsaver[two.points.size()];
-	int Threshold = 5;
-	std::vector <geometry_msgs::Point32 > firstpoints;
-	std::vector <geometry_msgs::Point32 > secondpoints;
-	for(int runthroughs = 0; runthroughs < Threshold ; runthroughs++){
-		double sum = 0;
-		 // SEARCH CLOSEST NEIGHBOR
-		 //-----------------------------------------------------------------------
-		 for(int i = 0; i < two.points.size();i++){
-			double prev = 1000000;
-			for(int z = 0; z < one.points.size();z++){
-				double length = sqrt(pow(one.points[z].x-two.points[i].x,2)+pow(one.points[z].y-two.points[i].y,2));
-				if(length == 0)
-					continue;
-				if(length < prev){
-					//First occurence of point pair
-					if(lengthsaver[i] == 0){
-						 //Save position of closest neighbor here
-						 lengthsaver[i] = length;
-						 saver[i] = z;
-					}
-					//If pair already exists
-					else if(contains(saver,z)){
-						if(lengthsaver[i] < length){
-						}
-						// If save length > current length
-						else{
-							lengthsaver[i] = length;
-					                saver[i] = z;
-						}
-					} else {
-						lengthsaver[i] = length;
-						saver[i] = z;
-					}
-					prev = length;
-				}
-			}
-		 }
-		for(int i = 0; i < two.points.size();i++){
-			
-		}*/
+    //while ! converging  			//START of the icp iterations
+    int secondsize = two.points.size();
+    int saver[two.points.size()];
+    double lengthsaver[two.points.size()];
+    int Threshold = 5;
+    std::vector <geometry_msgs::Point32 > firstpoints;
+    std::vector <geometry_msgs::Point32 > secondpoints;
+    for(int runthroughs = 0; runthroughs < Threshold ; runthroughs++){
+        double sum = 0;
+         // SEARCH CLOSEST NEIGHBOR
+         //-----------------------------------------------------------------------
+         for(int i = 0; i < two.points.size();i++){
+            double prev = 1000000;
+            for(int z = 0; z < one.points.size();z++){
+                double length = sqrt(pow(one.points[z].x-two.points[i].x,2)+pow(one.points[z].y-two.points[i].y,2));
+                if(length == 0)
+                    continue;
+                if(length < prev){
+                    //First occurence of point pair
+                    if(lengthsaver[i] == 0){
+                         //Save position of closest neighbor here
+                         lengthsaver[i] = length;
+                         saver[i] = z;
+                    }
+                    //If pair already exists
+                    else if(contains(saver,z)){
+                        if(lengthsaver[i] < length){
+                        }
+                        // If save length > current length
+                        else{
+                            lengthsaver[i] = length;
+                                    saver[i] = z;
+                        }
+                    } else {
+                        lengthsaver[i] = length;
+                        saver[i] = z;
+                    }
+                    prev = length;
+                }
+            }
+         }
+        for(int i = 0; i < two.points.size();i++){
+
+        }*/
